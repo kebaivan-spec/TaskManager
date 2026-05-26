d664b7eb57497dd9f02e7bc350baea08a6ef88b8
Ivan <keba.ivan@lll.kpi.ua>
2026-05-27 01:32:20 +0300

    Initial task manager

--- task_manager.py
+++ task_manager.py
@@ -1,43 +1,19 @@
-class TaskManager:
-    def __init__(self):
-        self.tasks = []
-
-    def add_task(self, task):
-        self.tasks.append(task)
-
-    def remove_task(self, task):
-        if task in self.tasks:
-            self.tasks.remove(task)
-
-    def show_tasks(self):
-        if not self.tasks:
-            print("Немає задач")
-        else:
-            for i, task in enumerate(self.tasks, 1):
-                print(f"{i}. {task}")
-
-
-manager = TaskManager()
+tasks = []
 
 while True:
 
-    print("\n1 - Додати задачу")
-    print("2 - Видалити задачу")
-    print("3 - Показати задачі")
-    print("4 - Вихід")
+    print("\n1 - Add task")
+    print("2 - Show tasks")
+    print("3 - Exit")
 
-    choice = input("Вибір: ")
+    choice = input("Choice: ")
 
     if choice == "1":
-        task = input("Введіть задачу: ")
-        manager.add_task(task)
+        task = input("Task: ")
+        tasks.append(task)
 
     elif choice == "2":
-        task = input("Яку задачу видалити?: ")
-        manager.remove_task(task)
+        print(tasks)
 
     elif choice == "3":
-        manager.show_tasks()
-
-    elif choice == "4":
         break
